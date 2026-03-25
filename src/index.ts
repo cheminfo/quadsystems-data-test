96d5a84c57bca9393f934eadcb114c6e1d103088
jobo322 <jose.bolanos@correounivalle.edu.co>
2026-03-25 02:24:18 -0500

    chore: use the filter directly in the constructor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { join } from 'node:path';
 
 import type { AbsolutePath } from 'data-test-api';
-import init from 'data-test-api';
+import { DataTestApi } from 'data-test-api';
 
 const path = join(import.meta.dirname, '../data') as AbsolutePath;
 
-export const quadsystemsFiles = init(
+export const quadsystemsFiles = new DataTestApi(
   path,
   (file) => file.isFile() && !file.name.startsWith('.'),
 );
